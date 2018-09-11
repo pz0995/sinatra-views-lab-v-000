e9688eee9863ed2810a337049fce90a78bf706c6
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-11 10:06:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ require 'erb'
 class App < Sinatra::Base
 
 	get '/hello' do
-		erb :index
+		erb :hello
 	end
 
 	get '/goodbye' do
-		erb :index
+		erb :goodbye
 	end
 
 	get '/date' do
-		erb :info
+		erb :date
 	end
-	
+
 end
